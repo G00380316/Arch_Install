0f005cc2aee070e8344b5924f26e349071aea749
Enoch Abiodun <ezea128@gmail.com>
2025-05-12 01:24:46 +0100

    Changed some keybindings for waybar and removed comments and changed some waybar related hyde scripts

--- configs/scripts/Hyde_Inject/Scripts/themeswitch.sh
+++ configs/scripts/Hyde_Inject/Scripts/themeswitch.sh
@@ -2,4 +2,10 @@
 
 echo "This script will be deprecated. Please use theme.switch.sh instead."
 scrDir="$(dirname "$(realpath "$0")")"
+scriptsDir="$HOME/.config/hypr/scripts"
+
 "${scrDir}"/theme.switch.sh "$@"
+pid=$!
+wait "$pid"
+
+"${scriptsDir}"/Refresh.sh
