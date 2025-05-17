4f1b4a48ede293ae0d92658d2204fae7a2598229
Enoch Abiodun <ezea128@gmail.com>
2025-05-17 02:21:15 +0100

    ags fixed

--- install-scripts/ags.sh
+++ install-scripts/ags.sh
@@ -71,8 +71,8 @@ printf "\n%.0s" {1..1}
 printf "${INFO} Kindly Standby...cloning and compiling ${SKY_BLUE}Aylur's GTK shell $ags_tag${RESET}...\n"
 printf "\n%.0s" {1..1}
 # Clone repository with the specified tag and capture git output into MLOG
-if git clone --depth=1 https://github.com/altG00380316/ags_v1.9.0; then
-    cd ags_v1.9.0 || exit 1
+if git clone --depth=1 https://github.com/G00380316/ags; then
+    cd ags || exit 1
     npm install
     meson setup build
     if sudo meson install -C build 2>&1 | tee -a "$MLOG"; then
