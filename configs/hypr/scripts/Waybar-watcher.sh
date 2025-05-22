6a3bb8546ebb8f9d6765fddcdb2d5ed2192926ef
Enoch Abiodun <ezea128@gmail.com>
2025-05-22 17:12:40 +0100

    Added Service to make sure waybar is running at all times

---
+++ configs/hypr/scripts/Waybar-watcher.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+# Interval between checks (in seconds)
+INTERVAL=0.5
+
+# Command to start Waybar
+START_CMD="waybar"
+
+while true; do
+    if ! pgrep -x "waybar" > /dev/null; then
+        echo "$(date): Waybar not running. Restarting..."
+        pkill -x waybar  # Kill any existing waybar processes (just in case)
+        sleep 1
+        $START_CMD &
+    else
+        # Make sure only one waybar process is running
+        WAYBAR_COUNT=$(pgrep -xc waybar)
+        if [ "$WAYBAR_COUNT" -gt 1 ]; then
+            echo "$(date): Multiple Waybar instances detected. Restarting clean..."
+            pkill -x waybar
+            sleep 1
+            $START_CMD &
+        fi
+    fi
+    sleep $INTERVAL
+done
+
